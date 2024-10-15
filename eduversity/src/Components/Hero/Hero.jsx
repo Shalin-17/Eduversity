ea0ebae9d358e64535a839f2bace1e994ddf8020
Shalin A <shalinasir23@gmail.com>
2024-10-15 10:09:42 +0530

    Eduversity Main page

---
+++ eduversity/src/Components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className="hero-text">
+        <h1 >
+          Empowering Minds Through Education
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
